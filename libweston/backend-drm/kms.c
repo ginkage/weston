2797162ea1008e4b0117b2c1fbe0373962f43f3e
Jeffy Chen <jeffy.chen@rock-chips.com>
2021-08-13 10:10:01 +0800

    backend-drm: Allow pageflip error
    Allow to recover from pageflip error:
    [02:08:14.089] queueing pageflip failed: Operation not permitted
    [02:08:14.090] Couldn't apply state for output eDP-1
    [02:08:14.090] repaint-flush failed: No such file or directory
    
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- libweston/backend-drm/kms.c
+++ libweston/backend-drm/kms.c
@@ -1680,6 +1680,7 @@ drm_pending_state_apply(struct drm_pending_state *pending_state)
 	struct drm_backend *b = device->backend;
 	struct drm_output_state *output_state, *tmp;
 	struct drm_crtc *crtc;
+	int has_error = 0;
 
 	if (wl_list_empty(&pending_state->output_list)) {
 		drm_pending_state_free(pending_state);
@@ -1736,6 +1737,7 @@ drm_pending_state_apply(struct drm_pending_state *pending_state)
 				drm_output_fini_egl(output);
 				drm_output_init_egl(output, b);
 			}
+			has_error = 1;
 		}
 	}
 
@@ -1743,7 +1745,7 @@ drm_pending_state_apply(struct drm_pending_state *pending_state)
 
 	drm_pending_state_free(pending_state);
 
-	return 0;
+	return has_error ? -EACCES : 0;
 }
 
 /**
@@ -1759,6 +1761,7 @@ drm_pending_state_apply_sync(struct drm_pending_state *pending_state)
 	struct drm_device *device = pending_state->device;
 	struct drm_output_state *output_state, *tmp;
 	struct drm_crtc *crtc;
+	int has_error = 0;
 
 	if (device->atomic_modeset)
 		return drm_pending_state_apply_atomic(pending_state,
@@ -1787,6 +1790,7 @@ drm_pending_state_apply_sync(struct drm_pending_state *pending_state)
 		if (ret != 0) {
 			weston_log("Couldn't apply state for output %s\n",
 				   output_state->output->base.name);
+			has_error = 1;
 		}
 	}
 
@@ -1794,7 +1798,7 @@ drm_pending_state_apply_sync(struct drm_pending_state *pending_state)
 
 	drm_pending_state_free(pending_state);
 
-	return 0;
+	return has_error ? -EACCES : 0;
 }
 
 void
